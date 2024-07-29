65e282163907b3dc595f0ecc26f3cabdfd9f3a0b
breeze <109904206+wisebreeze@users.noreply.github.com>
2024-07-29 11:06:51 +0000

    Update index.js files

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 import 'mdui/mdui.css'
-import "../res/css/simpleCrop.scss"
-import "../res/css/style.scss"
+import "../css/simpleCrop.scss"
+import "../css/style.scss"
 
-import icon from "../res/image/icon.png"
+import icon from "../image/icon.png"
 document.querySelector("link[rel='icon']").href=icon
 document.querySelector("link[rel='shortcut icon']").href=icon
 document.querySelectorAll("meta[name='apple-touch-icon-precomposed']").forEach(e=>e.href=icon)
