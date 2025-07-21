682f4f0ee3b4b6b29bf092d14570309613fdff7e
breeze <109904206+wisebreeze@users.noreply.github.com>
2025-07-21 11:08:55 +0000

    Configured SPA routing

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,28 @@ const TerserPlugin = require('terser-webpack-plugin');
 const distPath = path.resolve(__dirname, 'dist');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
+const pages = ['index','404'];
+function SPA(pages) {
+  let htmlPath='index.html';
+  return pages.map(page => {
+    if (page.endsWith('/'))htmlPath = page+"index.html";
+    else htmlPath=page.endsWith(".html")?page:page+".html";
+    return new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, "dist", htmlPath),
+      template: path.resolve(__dirname, "src", "index.html"),
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      },
+      chunks: [page]
+    });
+  });
+}
+
 module.exports = {
   mode: "production",// development
   devtool: "eval-cheap-module-source-map",
@@ -85,19 +107,7 @@ module.exports = {
       __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false)
     }),
     new VueLoaderPlugin(),
-    new HtmlWebpackPlugin({
-      filename: path.resolve(__dirname, "dist", 'index.html'),
-      template: path.resolve(__dirname, "src", "index.html"),
-      minify: {
-        collapseWhitespace: true,
-        removeComments: true,
-        removeRedundantAttributes: true,
-        removeScriptTypeAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        useShortDoctype: true
-      },
-      chunks: ["index"]
-    }),
+    ...SPA(pages),
     new TerserPlugin({
       extractComments: false,
       terserOptions: {
